a576d3a6b7892c97c731d49b06e5815bd1a07193
viktorybloom <viktorsulejic@gmail.com>
2026-05-02 11:46:39 +1000

    swaylock old and no longer maintained

--- env/.config/sway/scripts/lock.sh
+++ env/.config/sway/scripts/lock.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
-# Times the screen off and puts it to background
-swayidle -w \
-    timeout 300 'swaymsg "output * power off"' \
-    resume 'swaymsg "output * power on"' &
 
-swaylock -C ~/.config/swaylock/config
-
-kill%%
-
-
+hyprlock &
+sleep 10
+systemctl suspend
